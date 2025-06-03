37a4db546b60a0e9c9356807d2dc7b8df9bfe52c
roldan-gianrafael <gianrafaelroldan13@gmail.com>
2025-06-03 22:41:44 +0800

    Add changes to files in roldan branch

--- roldan.py
+++ roldan.py
@@ -1,61 +1,68 @@
 def border():
-    print("-" * 60)
+    print("========================================================="
+          "===========================")
 
 def show_roldan():
     while True:
         border()
-        print("Hello! I'm Gian Rafael B. Roldan.")
-        print("1. Basic Info")
+        print("\t\t\tHello! I'm Gian Rafael Roldan.")
+        border()
+        print("1. Personal Information")
         print("2. Goals")
         print("3. Hobbies")
         print("4. Motto")
         print("5. Exit")
         border()
-        print("Team Comments:")
+        print("\t\t\t\tTEAMMATES COMMENTS")
+        border()
         print("Bernas comment: Good, I like your code!")
         print("Buenacifra comment: Have fun coding!")
         print("Tero comment: Nice hobby!")
-        border()
+        
         choice = input("Please select an option (1-5): ")
-        border()
-
+        
         if choice == '1':
+            border()
+            print("\t\t\t\t\tPERSONAL INFORMATION")
             border()
             print(
                 "Name: Gian Rafael B. Roldan\n"
                 "Age: 20\n"
                 "Birthday: 08/08/2004\n"
                 "Address: New Lower Bicutan, Taguig\n"
-                "School: PUP-Taguig\n"
-                "Course: BSIT"
-            )
-            border()
+                "School: PUP-Taguig\n")
+            input("Press 'Enter' to proceed.")
             continue
         elif choice == '2':
             border()
+            print("\t\t\t\t\tGOALS")
+            border()
             print("My goal is winning in silence, "
                   "living in peace.")
-            border()
+            input("Press 'Enter' to proceed.")
             continue
         elif choice == '3':
+            border()
+            print("\t\t\t\t\tHOBBIES")
             border()
             print("I enjoy playing basketball, "
                   "nature tripping, "
                   "and exploring different cities.")
-            border()
+            input("Press 'Enter' to proceed.")
             continue
         elif choice == '4':
             border()
+            print("\t\t\t\t\tMOTTO")
+            border()
             print("My motto is: 'real eyes "
                   "realize real lies.'")
-            border()
+            input("Press 'Enter' to proceed.")
             continue
         elif choice == '5':
             border()
-            print("Good bye!")
+            print("Exiting...")
             border()
             break
-        print("\nInvalid option. Please try again.")
+        print("Invalid option. Please try again.") 
         continue
-
 roldan = show_roldan()
