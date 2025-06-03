156ebbd277d30acace7e65fb8bfd171d137a80f4
Buenacifra, Abrianne <abriannebuenacifra@gmail.com>
2025-06-03 21:40:16 +0800

    PR Buenacifra Module (#2)
    * Add buenacifra module
    
    * add bernas comment
    
    * add roldan comments
    
    * add comment to buenacifra-module
    
    * Added changes to buenacifra module
    
    * Add changes to files in buenacifra branch
    
    ---------
    
    Co-authored-by: bernas-ernesto <thonbernas1134@gmail.com>
    Co-authored-by: roldan-gianrafael <gianrafaelroldan13@gmail.com>
    Co-authored-by: altheno <ishigamisenku8888@gmail.com>
    Co-authored-by: Bernas-Ernesto <95688601+Bernas-Ernesto@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 from bernas import show_bernas
-# TODO: Import Buenacifra module
+from buenacifra import show_buenacifra
 # TODO: Import Roldan module
 # TODO: Import Tero module
 
-# TODO: MAIN MENU
+def main_menu():
+    while True:
+        print("\nWelcome to the Main Menu!")
+        print("1. Ernesto Bernas III")
+        print("2. Abrianne Buenacifra")
+        print("3. Gian Rafael Roldan")
+        print("4. Altheno Mari Tero")
+        print("5. Exit")
+
+        choice = input("Please select an option (1-5): ")
+
+        if choice == '1':
+            
+        elif choice == '2':
+            show_buenacifra()
+            continue
+        elif choice == '3':
+
+        elif choice == '4':
+
+        elif choice == '5':
+            print("Exiting...")
+            break
+        print("Invalid choice. Please try again.")
+        continue
+
+main_menu()
